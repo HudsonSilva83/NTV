db5d7c59f0da8fe6db59602b9f7e8d9c5035c312
Hudson <hu.psilva@gmail.com>
2022-07-29 10:32:54 -0300

    teste amazon

--- src/test/java/br/com/iptv/auto/iptvAutoTest.java
+++ src/test/java/br/com/iptv/auto/iptvAutoTest.java
@@ -34,7 +34,8 @@ public class iptvAutoTest {
 		FirefoxOptions options = new FirefoxOptions();
 		options.addArguments("--headless");
 		options.addArguments("-user-agent=\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36\"");
-		options.addArguments("--window-size=1200,800");
+		//options.addArguments("--window-size=1200,800");
+		options.addArguments("--window-size=1080,1000");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
 		
@@ -62,249 +63,276 @@ public class iptvAutoTest {
 	@SuppressWarnings("deprecation")
 	@Test
 	public void teste() throws InterruptedException {
+		
+		
+		driver.get("https://alexa.amazon.com.br/");
 
-		driver.get("https://pltf.vip/login");
+		//amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
+		
+		//driver.manage().window().setSize(d);
+		Thread.sleep(2000);
+		amazonHomePage.usu();
+		amazonHomePage.s();
+		amazonHomePage.b();
+		Thread.sleep(9000);
+		amazonHomePage.clicarMenuLembretesAlarme();
+		Thread.sleep(5000);
+		amazonHomePage.butonAdicionarLembretes();
+		amazonHomePage
+				.escreverlembrete("A expiração do IPETÊVÊ é 28/08/2022 porém o saldo é de 3");
+		amazonHomePage.clicarData();
+		Thread.sleep(3000);
 
-		homePage = new iptvHomePage(driver);
-		amazonHomePage = new AmazonHomePage(driver);
-		Thread.sleep(6000);
-		homePage.preeencherUsuario();
-		homePage.preeencherSenha();
-		homePage.botaoLogar();
+		amazonHomePage.clicarHora();
+		Thread.sleep(1000);
+		amazonHomePage.clicarHora();
+		Thread.sleep(1000);
 
-		Thread.sleep(7000);
-		String fichas = homePage.obterFichas();
-		int ficha = Integer.parseInt(fichas);
-		homePage.acessarMenuListas();
+		Date dataHoraAtual = new Date();
 
-		String dataExpi = homePage.obterDataExpiracao();
-		String dataExpiracao = dataExpi.substring(0, 10);
-		// DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy/MM/dd
-		// HH:mm:ss");
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		String dataAtual = dtf.format(LocalDateTime.now());
+		String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
 
-		ficha = 0;
+		dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
+		String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
+		System.out.println("Aqui está a hora" + horaA);
 
-		if (dataExpiracao != dataAtual && ficha == 0) {
+		amazonHomePage.setarHora(horaA);
+		Thread.sleep(1000);
+		Thread.sleep(1000);
+		amazonHomePage.clicaComboselect();
+		Thread.sleep(1000);
+		amazonHomePage.select();
+		Thread.sleep(1000);
+		amazonHomePage.butonSalvar();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
-			driver.get("https://alexa.amazon.com.br/");
-
-			//amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
-			
-			driver.manage().window().setSize(d);
-			Thread.sleep(2000);
-			amazonHomePage.usu();
-			amazonHomePage.s();
-			amazonHomePage.b();
-			Thread.sleep(9000);
-			amazonHomePage.clicarMenuLembretesAlarme();
-			Thread.sleep(5000);
-			amazonHomePage.butonAdicionarLembretes();
-			amazonHomePage
-					.escreverlembrete("A expiração do IPETÊVÊ é " + dataExpiracao + " porém o saldo é de " + ficha);
-			amazonHomePage.clicarData();
-			// Thread.sleep(3000);
-			// amazonHomePage.enterData();
-			Thread.sleep(3000);
-
-			amazonHomePage.clicarHora();
-			Thread.sleep(1000);
-			amazonHomePage.clicarHora();
-			Thread.sleep(1000);
-
-			Date dataHoraAtual = new Date();
-
-			// String data = new
-			// SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-			String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
-			dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
-			String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
-			System.out.println("Aqui está a hora" + horaA);
-
-			amazonHomePage.setarHora(horaA);
-			Thread.sleep(1000);
-			// amazonHomePage.enterHora();
-			Thread.sleep(1000);
-			amazonHomePage.clicaComboselect();
-			Thread.sleep(1000);
-			amazonHomePage.select();
-			Thread.sleep(1000);
-			amazonHomePage.butonSalvar();
-
-		} else {
-
-			if (dataExpiracao.equals(dataAtual)) {
-
-				System.out.println("data sao iguais");
-
-				if (ficha > 0) {
-
-					Thread.sleep(3000);
-					homePage.clicarBotaoNilson();
-					Thread.sleep(1000);
-					homePage.menuRenovar();
-					Thread.sleep(5000);
-
-					// https://stackoverflow.com/questions/13936167/how-to-deal-with-modaldialog-using-selenium-webdriver
-					driver.switchTo().activeElement();
-					Thread.sleep(1000);
-					homePage.scrollar3();
-
-					Thread.sleep(2000);
-
-					// homePage.comboSelecionar();
-					Thread.sleep(2000);
-
-					// homePage.selecionarPlano25();
-
-					// homePage.quantidade();
-
-					homePage.botaoRenovar();
-
-					amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
-
-					Thread.sleep(1000);
-
-					
-					Thread.sleep(2000);
-					amazonHomePage.usu();
-					amazonHomePage.s();
-					amazonHomePage.b();
-					driver.manage().window().setSize(d);
-					Thread.sleep(9000);
-					amazonHomePage.clicarMenuLembretesAlarme();
-					Thread.sleep(5000);
-					amazonHomePage.butonAdicionarLembretes();
-					amazonHomePage
-							.escreverlembrete("Renovado os créditos para Nilson agora o saldo é de " + (ficha - 1));
-					amazonHomePage.clicarData();
-					// Thread.sleep(3000);
-					// amazonHomePage.enterData();
-					Thread.sleep(3000);
-
-					amazonHomePage.clicarHora();
-					Thread.sleep(1000);
-					amazonHomePage.clicarHora();
-					Thread.sleep(500);
-
-					Date dataHoraAtual = new Date();
-
-					// String data = new
-					// SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-					String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
-					dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
-					String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
-					System.out.println("Aqui está a hora" + horaA);
-
-					amazonHomePage.setarHora(horaA);
-					Thread.sleep(500);
-					// amazonHomePage.enterHora();
-					Thread.sleep(500);
-					amazonHomePage.clicaComboselect();
-					Thread.sleep(500);
-					amazonHomePage.select();
-					Thread.sleep(1000);
-					amazonHomePage.butonSalvar();
-
-				} else {
-
-					amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
-					driver.manage().window().setSize(d);
-
-					Thread.sleep(1000);
-
-					amazonHomePage.usu();
-					amazonHomePage.s();
-					amazonHomePage.b();
-					Thread.sleep(9000);
-					amazonHomePage.clicarMenuLembretesAlarme();
-					Thread.sleep(5000);
-					amazonHomePage.butonAdicionarLembretes();
-					amazonHomePage.escreverlembrete("Acabaram os créditos do IPETÊVÊ saldo é de " + ficha);
-					amazonHomePage.clicarData();
-					// Thread.sleep(3000);
-					// amazonHomePage.enterData();
-					Thread.sleep(3000);
-
-					amazonHomePage.clicarHora();
-					Thread.sleep(1000);
-					amazonHomePage.clicarHora();
-					Thread.sleep(500);
-
-					Date dataHoraAtual = new Date();
-
-					// String data = new
-					// SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-					String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
-					dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
-					String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
-					System.out.println("Aqui está a hora" + horaA);
-
-					amazonHomePage.setarHora(horaA);
-					Thread.sleep(500);
-					// amazonHomePage.enterHora();
-					Thread.sleep(500);
-					amazonHomePage.clicaComboselect();
-					Thread.sleep(500);
-					amazonHomePage.select();
-					Thread.sleep(1000);
-					amazonHomePage.butonSalvar();
-
-				}
-
-			} else {
-
-				amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
-
-				driver.manage().window().setSize(d);
-
-				Thread.sleep(1000);
-				amazonHomePage.usu();
-				amazonHomePage.s();
-				amazonHomePage.b();
-				Thread.sleep(9000);
-				amazonHomePage.clicarMenuLembretesAlarme();
-				Thread.sleep(5000);
-				amazonHomePage.butonAdicionarLembretes();
-				amazonHomePage.escreverlembrete(
-						"A data de expiração será " + dataExpiracao + " e têm " + ficha + " créditos no momento");
-				amazonHomePage.clicarData();
-				// Thread.sleep(3000);
-				// amazonHomePage.enterData();
-				Thread.sleep(3000);
-
-				amazonHomePage.clicarHora();
-				Thread.sleep(1000);
-				amazonHomePage.clicarHora();
-				Thread.sleep(500);
-
-				Date dataHoraAtual = new Date();
-
-				// String data = new
-				// SimpleDateFormat("dd/MM/yyyy").format(dataHoraAtual);
-				String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
-
-				dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
-				String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
-				// System.out.println("Aqui está a hora" + horaA);
-
-				amazonHomePage.setarHora(horaA);
-				Thread.sleep(500);
-				// amazonHomePage.enterHora();
-				Thread.sleep(500);
-				amazonHomePage.clicaComboselect();
-				Thread.sleep(500);
-				amazonHomePage.select();
-				Thread.sleep(1000);
-				amazonHomePage.butonSalvar();
-
-			}
-
-		}
+//		driver.get("https://pltf.vip/login");
+//
+//		homePage = new iptvHomePage(driver);
+//		amazonHomePage = new AmazonHomePage(driver);
+//		Thread.sleep(6000);
+//		homePage.preeencherUsuario();
+//		homePage.preeencherSenha();
+//		homePage.botaoLogar();
+//
+//		Thread.sleep(7000);
+//		String fichas = homePage.obterFichas();
+//		int ficha = Integer.parseInt(fichas);
+//		homePage.acessarMenuListas();
+//
+//		String dataExpi = homePage.obterDataExpiracao();
+//		String dataExpiracao = dataExpi.substring(0, 10);
+//		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//		String dataAtual = dtf.format(LocalDateTime.now());
+//
+//		ficha = 0;
+//
+//		if (dataExpiracao != dataAtual && ficha == 0) {
+//
+//			driver.get("https://alexa.amazon.com.br/");
+//
+//			//amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
+//			
+//			driver.manage().window().setSize(d);
+//			Thread.sleep(2000);
+//			amazonHomePage.usu();
+//			amazonHomePage.s();
+//			amazonHomePage.b();
+//			Thread.sleep(9000);
+//			amazonHomePage.clicarMenuLembretesAlarme();
+//			Thread.sleep(5000);
+//			amazonHomePage.butonAdicionarLembretes();
+//			amazonHomePage
+//					.escreverlembrete("A expiração do IPETÊVÊ é " + dataExpiracao + " porém o saldo é de " + ficha);
+//			amazonHomePage.clicarData();
+//			Thread.sleep(3000);
+//
+//			amazonHomePage.clicarHora();
+//			Thread.sleep(1000);
+//			amazonHomePage.clicarHora();
+//			Thread.sleep(1000);
+//
+//			Date dataHoraAtual = new Date();
+//
+//			String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+//
+//			dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
+//			String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
+//			System.out.println("Aqui está a hora" + horaA);
+//
+//			amazonHomePage.setarHora(horaA);
+//			Thread.sleep(1000);
+//			Thread.sleep(1000);
+//			amazonHomePage.clicaComboselect();
+//			Thread.sleep(1000);
+//			amazonHomePage.select();
+//			Thread.sleep(1000);
+//			amazonHomePage.butonSalvar();
+//
+//		} else {
+//
+//			if (dataExpiracao.equals(dataAtual)) {
+//
+//				System.out.println("data sao iguais");
+//
+//				if (ficha > 0) {
+//
+//					Thread.sleep(3000);
+//					homePage.clicarBotaoNilson();
+//					Thread.sleep(1000);
+//					homePage.menuRenovar();
+//					Thread.sleep(5000);
+//
+//					// https://stackoverflow.com/questions/13936167/how-to-deal-with-modaldialog-using-selenium-webdriver
+//					driver.switchTo().activeElement();
+//					Thread.sleep(1000);
+//					homePage.scrollar3();
+//
+//					Thread.sleep(2000);
+//
+//					// homePage.comboSelecionar();
+//					Thread.sleep(2000);
+//
+//					// homePage.selecionarPlano25();
+//					// homePage.quantidade();
+//
+//					homePage.botaoRenovar();
+//
+//					amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
+//
+//					Thread.sleep(1000);
+//
+//					
+//					Thread.sleep(2000);
+//					amazonHomePage.usu();
+//					amazonHomePage.s();
+//					amazonHomePage.b();
+//					driver.manage().window().setSize(d);
+//					Thread.sleep(9000);
+//					amazonHomePage.clicarMenuLembretesAlarme();
+//					Thread.sleep(5000);
+//					amazonHomePage.butonAdicionarLembretes();
+//					amazonHomePage
+//							.escreverlembrete("Renovado os créditos para Nilson agora o saldo é de " + (ficha - 1));
+//					amazonHomePage.clicarData();
+//					Thread.sleep(3000);
+//
+//					amazonHomePage.clicarHora();
+//					Thread.sleep(1000);
+//					amazonHomePage.clicarHora();
+//					Thread.sleep(500);
+//
+//					Date dataHoraAtual = new Date();
+//
+//					String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+//
+//					dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
+//					String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
+//					System.out.println("Aqui está a hora" + horaA);
+//
+//					amazonHomePage.setarHora(horaA);
+//					Thread.sleep(500);
+//					amazonHomePage.clicaComboselect();
+//					Thread.sleep(500);
+//					amazonHomePage.select();
+//					Thread.sleep(1000);
+//					amazonHomePage.butonSalvar();
+//
+//				} else {
+//
+//					amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
+//					driver.manage().window().setSize(d);
+//
+//					Thread.sleep(1000);
+//
+//					amazonHomePage.usu();
+//					amazonHomePage.s();
+//					amazonHomePage.b();
+//					Thread.sleep(9000);
+//					amazonHomePage.clicarMenuLembretesAlarme();
+//					Thread.sleep(5000);
+//					amazonHomePage.butonAdicionarLembretes();
+//					amazonHomePage.escreverlembrete("Acabaram os créditos do IPETÊVÊ saldo é de " + ficha);
+//					amazonHomePage.clicarData();
+//					Thread.sleep(3000);
+//
+//					amazonHomePage.clicarHora();
+//					Thread.sleep(1000);
+//					amazonHomePage.clicarHora();
+//					Thread.sleep(500);
+//
+//					Date dataHoraAtual = new Date();
+//					String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+//
+//					dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
+//					String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
+//					System.out.println("Aqui está a hora" + horaA);
+//
+//					amazonHomePage.setarHora(horaA);
+//					Thread.sleep(500);
+//					Thread.sleep(500);
+//					amazonHomePage.clicaComboselect();
+//					Thread.sleep(500);
+//					amazonHomePage.select();
+//					Thread.sleep(1000);
+//					amazonHomePage.butonSalvar();
+//
+//				}
+//
+//			} else {
+//
+//				amazonHomePage.acessarPaginaAmazon(1, "https://alexa.amazon.com.br/");
+//
+//				driver.manage().window().setSize(d);
+//
+//				Thread.sleep(1000);
+//				amazonHomePage.usu();
+//				amazonHomePage.s();
+//				amazonHomePage.b();
+//				Thread.sleep(9000);
+//				amazonHomePage.clicarMenuLembretesAlarme();
+//				Thread.sleep(5000);
+//				amazonHomePage.butonAdicionarLembretes();
+//				amazonHomePage.escreverlembrete(
+//						"A data de expiração será " + dataExpiracao + " e têm " + ficha + " créditos no momento");
+//				amazonHomePage.clicarData();
+//				Thread.sleep(3000);
+//
+//				amazonHomePage.clicarHora();
+//				Thread.sleep(1000);
+//				amazonHomePage.clicarHora();
+//				Thread.sleep(500);
+//
+//				Date dataHoraAtual = new Date();
+//
+//				String hora = new SimpleDateFormat("HH:mm:ss").format(dataHoraAtual);
+//
+//				dataHoraAtual.setMinutes(dataHoraAtual.getMinutes() + 3);
+//				String horaA = new SimpleDateFormat("HH:mm").format(dataHoraAtual);
+//
+//				amazonHomePage.setarHora(horaA);
+//				Thread.sleep(500);
+//				amazonHomePage.clicaComboselect();
+//				Thread.sleep(500);
+//				amazonHomePage.select();
+//				Thread.sleep(1000);
+//				amazonHomePage.butonSalvar();
+//
+//			}
+//
+//		}
 
 	}
 
