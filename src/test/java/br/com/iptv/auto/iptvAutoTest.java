da6edff9caac5ca08d76bd51cd35f2a6c5f5716c
Hudson <hu.psilva@gmail.com>
2022-07-31 11:28:34 -0300

    teste

--- src/test/java/br/com/iptv/auto/iptvAutoTest.java
+++ src/test/java/br/com/iptv/auto/iptvAutoTest.java
@@ -58,7 +58,7 @@ public class iptvAutoTest {
 		options.addArguments("--disable-dev-shm-usage");
 		options.addArguments("--headless");
 		options.addArguments("--window-size=1200,800");
-		 options.addArguments("--user-agent=\"Mozilla/103.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36\"");
+		 options.addArguments("--user-agent=\"Mozilla/99.0.4844.51 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36\"");
 		driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		//driver.manage().window().maximize();
