c5b588763d1fdca1a407518cd161d889bb20a93e
Hudson <hu.psilva@gmail.com>
2022-07-31 12:37:24 -0300

    teste

--- src/test/java/br/com/iptv/auto/iptvAutoTest.java
+++ src/test/java/br/com/iptv/auto/iptvAutoTest.java
@@ -43,7 +43,7 @@ public class iptvAutoTest {
 
 		FirefoxOptions options = new FirefoxOptions();
 		options.addArguments("--headless");
-		options.addArguments("--user-agent=\"Mozilla/84.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36\"");
+		//options.addArguments("--user-agent=\"Mozilla/84.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36\"");
 		//options.addArguments("--window-size=1200,800");
 		options.addArguments("--window-size=1920,1080");
 		options.addArguments("--no-sandbox");
